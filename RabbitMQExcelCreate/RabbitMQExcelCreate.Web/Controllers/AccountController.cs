3ebea4d95f2d5e60c6e202d51b288f472e204cda
yucelaydin <yucel@4alabs.io>
2023-06-05 16:28:39 +0300

    Identity

---
+++ RabbitMQExcelCreate/RabbitMQExcelCreate.Web/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace RabbitMQExcelCreate.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _singInManager;
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> singInManager)
+        {
+            _userManager = userManager;
+            _singInManager = singInManager;
+        }
+
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(string Email, string Password)
+        {
+            var hasUser = await _userManager.FindByEmailAsync(Email);
+            if (hasUser==null)
+            {
+                return View();
+            }
+            var signInResult=await _singInManager.PasswordSignInAsync(hasUser, Password,true,false);
+            if (!signInResult.Succeeded)
+            {
+                return View();
+            }
+            return RedirectToAction(nameof(HomeController.Index),"Home");
+        }
+    }
+}
---
+++ RabbitMQExcelCreate/RabbitMQExcelCreate.Web/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RabbitMQExcelCreate.Web.Controllers
+{
+    [Authorize]
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
