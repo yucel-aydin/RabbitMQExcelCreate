9d7391335ac5a841d0c9107b57d82c0c578c09f0
yucelaydin <yucel@4alabs.io>
2023-06-05 15:56:01 +0300

    initial

---
+++ RabbitMQExcelCreate/RabbitMQExcelCreate.Web/Program.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using RabbitMQExcelCreate.Web.Models;
+
+namespace RabbitMQExcelCreate.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+            });
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>(opt =>
+            {
+                opt.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<AppDbContext>();
+
+            var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var appDbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+                appDbContext.Database.Migrate();
+                if (!appDbContext.Users.Any())
+                {
+                    userManager.CreateAsync(new IdentityUser()
+                    {
+                        UserName = "deneme1",
+                        Email = "deneme1@gmail.com",
+                    }, "Password12*").Wait();
+                    userManager.CreateAsync(new IdentityUser()
+                    {
+                        UserName = "deneme2",
+                        Email = "deneme2@gmail.com",
+                    }, "Password12*").Wait();
+                }
+            }
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
