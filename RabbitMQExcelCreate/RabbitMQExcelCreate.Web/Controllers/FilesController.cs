dbf8160d87c8d66ba790c66055c6397b9e3fa2d3
yucelaydin <yucel@4alabs.io>
2023-06-06 10:42:34 +0300

    Upload method add

---
+++ RabbitMQExcelCreate/RabbitMQExcelCreate.Web/Controllers/FilesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RabbitMQExcelCreate.Web.Models;
+
+namespace RabbitMQExcelCreate.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public FilesController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file, int fileId)
+        {
+            if (file is not { Length: > 0 }) return BadRequest();
+
+            var userFile = await _appDbContext.UserFiles.FirstAsync(x => x.Id == fileId);
+
+            var filePath=userFile.FileName + Path.GetExtension(file.FileName);
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/files", filePath);
+
+            using FileStream stream = new(path, FileMode.Create);
+            await file.CopyToAsync(stream);
+            userFile.CreatedDate = DateTime.Now;
+            userFile.FilePath = filePath;
+            userFile.FileStatus = FileStatus.Completed;
+
+            await _appDbContext.SaveChangesAsync();
+            return Ok();
+
+        }
+    }
+}
